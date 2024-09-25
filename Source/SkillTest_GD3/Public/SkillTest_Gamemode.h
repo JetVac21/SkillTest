70a858f967904e6d4474743614b89e7330c1967b
Mystos21 <122263716+JetVac21@users.noreply.github.com>
2024-09-25 16:36:46 +0200

    first

---
+++ Source/SkillTest_GD3/Public/SkillTest_Gamemode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "SkillTest_Gamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKILLTEST_GD3_API ASkillTest_Gamemode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SkillTest_GD3/Public/SkillTest_PlayerControler.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SkillTest_PlayerControler.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKILLTEST_GD3_API ASkillTest_PlayerControler : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
